4bbff9b0849036dce2d2ff8e1e4a082048099227
Moeed Ahmad <email@moeed.com>
2026-06-05 13:59:45 +0100

    Initial Commit

---
+++ Blue Bubble Vault/Blue_Bubble_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blue_Bubble_VaultApp.swift
+//  Blue Bubble Vault
+//
+//  Created by Moeed Ahmad on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Blue_Bubble_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blue Bubble Vault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blue Bubble Vault
+//
+//  Created by Moeed Ahmad on 6/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
